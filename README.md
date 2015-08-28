1c2d47a8385caa65077bf4da74f1af101010b1cf
Jon Rafkind <jon@rafkind.com>
2015-08-27 17:04:25 -0700

    can send directories

--- README.md
+++ README.md
@@ -12,6 +12,34 @@ portal.py sends files across the filesystem using a global fifo. The utility is 
 	/x/y/z $ ls
 	file1 file2
 
+Files passed to portal.py directory will be sent using the basename of the file. That is, any preceeding pathname that is part of the file will be removed and only the filename itself will be sent.
+
+    $ portal.py /var/log/auth.log
+	# The receiver will get a filename called 'auth.log'
+	# /var/log will be removed from the path
+
+Directories can be sent with portal. The directory structure and all files will also be sent in-tact.
+
+# Sender
+    /a/b/c/dir1
+	|------ dir2/
+	|------ dir2/x
+	|------ y
+
+    /a/b/c $ portal.py dir1
+	Sending dir1
+    /a/b/c $
+
+# Receiver
+    /x/y/z $ portal.py
+	Reading file dir2/x size 23
+	Reading file y size 9
+
+	/x/y/z/dir1
+	|------ dir2/
+	|------ dir2/x
+	|------ y
+
 An alternative method is to copy the source file to /, then move it to the destination.
 
 # Sender
--- portal.py
+++ portal.py
@@ -14,10 +14,25 @@ def size(path):
     import os
     return os.stat(path).st_size
 
-def create_json(files):
+def create_json(paths):
     import json
+    import os.path
 
-    data = {'files': [{filename(v): size(v)} for v in files]}
+    files = []
+
+    for v in paths:
+        if os.path.isdir(v):
+            for root, dirs, filenames in os.walk(v):
+                for dir in dirs:
+                    files.append({os.path.join(root, dir): -1})
+                for file in filenames:
+                    full = os.path.join(root, file)
+                    files.append({full: size(full)})
+        else:
+            files.append({filename(v): size(v)})
+
+    # data = {'files': [{filename(v): size(v)} for v in paths]}
+    data = {'files': files}
     out = json.JSONEncoder().encode(data)
     # print out
 
@@ -53,6 +68,7 @@ def read_json(pipe):
     text = read_json_data(pipe)
     # print text
     import json
+    import os
     decoder = json.JSONDecoder()
     out, ignore = decoder.raw_decode(text)
     # print "Read json " + str(out)
@@ -60,21 +76,26 @@ def read_json(pipe):
         path = file.keys()[0]
         size = file[path]
         print "Reading file %s size %d" % (path, size)
-        f = open(path, 'w')
-        bytes = pipe.read(size)
-        f.write(bytes)
-        f.close()
+        if size == -1:
+            os.makedirs(path)
+        else:
+            f = open(path, 'w')
+            bytes = pipe.read(size)
+            f.write(bytes)
+            f.close()
 
 import sys
 if len(sys.argv) > 1:
+    import os.path
     fifo = make_fifo('w')
     json = create_json(sys.argv[1:])
     send(fifo, json)
     fifo.write(bytearray([magic_delimiter]))
     for path in sys.argv[1:]:
         print "Sending %s" % path
-        bytes = bytearray(read_file(path))
-        send(fifo, bytes)
+        if os.path.isfile(path):
+            bytes = bytearray(read_file(path))
+            send(fifo, bytes)
     fifo.close()
 else:
     fifo = make_fifo('r')
